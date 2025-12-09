6c50dbb27e60c580224b89d4ff44904ceaf59dab
wang <5b1g0023@stuts.edu.tw>
2025-12-09 11:26:06 +0800

    aaa

--- main_time.py
+++ main_time.py
@@ -11,6 +11,10 @@ w, h = 1000, 750
 detect, alert = 5, 3
 cap = cv2.VideoCapture(0)
 
+detect_start_time = None
+alert_start_time = None
+current_timer_state = "DETECT"
+
 with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as pose:
 
     while cap.isOpened():
@@ -29,47 +33,64 @@ with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as p
          left_distence, right_distence) = DrawUtil.frame(img_bgr, results, w, h)
 
         now = dt.now().strftime('%Y-%m-%d %A %H:%M:%S')
+
+
+        ''' ---------- 判斷吃藥動作 ---------- '''
+        is_eating_medicine = False
+        if left_elbow_angle < 50 and left_distence < 150 :
+            is_eating_medicine = True
+            cv2.putText(DrawUtil_img_bgr, "Eat Meduicine", (50, 350), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 0, 0), 15)
+        elif right_elbow_angle < 50 and right_distence < 150 :
+            is_eating_medicine = True
+            cv2.putText(DrawUtil_img_bgr, "Eat Meduicine", (50, 350), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 0, 0), 15)
+        else:
+            pass
+        
         try:
             ''' ---------- 計時 ---------- '''
-            if detect > 0:
-                while detect >= 0:
-                    mins, secs = divmod(detect, 60)
-                    detect_timer = '{:02d}:{:02d}'.format(mins, secs)
-                    # print(timer, end='\r')
-                    print(detect_timer)
-                    cv2.putText(DrawUtil_img_bgr, detect_timer, (450, 300), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
-                    t.sleep(1)
-                    detect -= 1
-                print("END 1", end='\n\n')
-                cv2.putText(DrawUtil_img_bgr, "!!! detect TIMER STOP !!!", (50, 300), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
+            current_time = t.time()
+            if current_timer_state == "DETECT":
+                if detect_start_time is None:
+                    detect_start_time = current_time
 
-                if detect <= 0 and alert > 0:
-                    while alert >= 0:
-                        mins, secs = divmod(alert, 60)
-                        alert_timer = '{:02d}:{:02d}'.format(mins, secs)
-                        # print(timer, end='\r')
-                        print(alert_timer)
-                        cv2.putText(DrawUtil_img_bgr, alert_timer, (50, 300), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
-                        t.sleep(1)
-                        alert -= 1
-                    print("END 2", end='\n\n')
-                    cv2.putText(DrawUtil_img_bgr, "!!! alert TIMER STOP !!!", (50, 300), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
-                else:
-                    cv2.putText(DrawUtil_img_bgr, "!!! alert TIMER ERROR !!!", (50, 300), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
-            else:
-                cv2.putText(DrawUtil_img_bgr, "!!! detect TIMER ERROR !!!", (50, 300), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4  )
+                elapsed_time = current_time - detect_start_time
+                remaining_time = max(0, detect - round(elapsed_time))
 
+                min, secs = divmod(remaining_time, 60)
+                detect_timer = '{:02d}:{:02d}'.format(min, secs)
+                cv2.putText(DrawUtil_img_bgr, "Detect:", (390, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
+                cv2.putText(DrawUtil_img_bgr, detect_timer, (430, 130), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 4)
 
-            ''' ---------- 判斷吃藥動作 ---------- '''
-            if left_elbow_angle < 50 and left_distence < 150 :
-                cv2.putText(DrawUtil_img_bgr, "Eat Meduicine", (50, 350), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 0, 0), 15)
-            elif right_elbow_angle < 50 and right_distence < 150 :
-                cv2.putText(DrawUtil_img_bgr, "Eat Meduicine", (50, 350), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 0, 0), 15)
-            else:
-                pass
-            
+                if is_eating_medicine:
+                    current_timer_state = "DETECT"
+                    detect_start_time = None
+                elif remaining_time <= 0 and not is_eating_medicine:
+                    print("END 1 - Detect Timer Finished", end='\n\n')
+                    current_timer_state = "ALERT"
+                    alert_start_time = None
+
+            elif current_timer_state == "ALERT":
+                if alert_start_time is None:
+                    alert_start_time = current_time
+
+                elapsed_time = current_time - alert_start_time
+                remaining_time = max(0, alert - round(elapsed_time))
+
+                min, secs = divmod(remaining_time, 60)
+                alert_timer = '{:02d}:{:02d}'.format(min, secs)
+                cv2.putText(DrawUtil_img_bgr, "Alert:", (423, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
+                cv2.putText(DrawUtil_img_bgr, alert_timer, (430, 130), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 4)
+
+                if is_eating_medicine:
+                    current_timer_state = "DETECT"
+                    detect_start_time = None
+                elif remaining_time <= 0 and not is_eating_medicine:
+                    print("END 2 - Alert Timer Finished", end='\n\n')
+                    current_timer_state = "DETECT"
+                    detect_start_time = None
+
             ''' ---------- 顯示目前時間 ---------- '''
-            cv2.putText(DrawUtil_img_bgr, f"{now}", (50, 350), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 0), 5)
+            cv2.putText(DrawUtil_img_bgr, f"{now}", (120, 200), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 0), 5)
 
             ''' ---------- 顯示畫面 ---------- '''
             cv2.imshow("OOOOKKKK", DrawUtil_img_bgr)
