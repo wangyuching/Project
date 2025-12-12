a3c8680d4e9d5ad3b9ada7fbc4c6e1673fbdefef
王昱晴 <5b1g0023@stust.edu.tw>
2025-12-12 23:52:49 +0800

    Update IsEatMedcine.py

--- IsEatMedcine.py
+++ IsEatMedcine.py
@@ -4,3 +4,17 @@
         if left_elbow_angle < 50 and left_distence < 150 :
             is_eating_medicine = True right_distence < 150 :
             is_eating_medicine = True
+
+'''
+吃藥計數：
+第一次偵測到吃藥動作，
+count += 1
+倒計時10s；
+    在 10s 內有動作：
+        count +=1
+        cv2.putText("finish medcine")
+        count == 0
+        break to 定時
+    在 10s 內無動作：
+'''
+        break
