7b8fc7466d9dfb20b5b7f59ec469c5d5de772303
王昱晴 <5b1g0023@stust.edu.tw>
2025-12-12 23:48:02 +0800

    Create IsEatMedcine.py

---
+++ IsEatMedcine.py
@@ -0,0 +1,6 @@
+''' ---------- 判斷有無吃藥動作 ---------- '''
+        is_eating_medicine = False
+
+        if left_elbow_angle < 50 and left_distence < 150 :
+            is_eating_medicine = True right_distence < 150 :
+            is_eating_medicine = True
