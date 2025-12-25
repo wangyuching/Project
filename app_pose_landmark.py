bf8a5a94364429b93c7d135018bc5550e9873be5
wang <5b1g0023@stuts.edu.tw>
2025-12-25 11:55:07 +0800

    do combing

---
+++ app_pose_landmark.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, Response
+import cv2
+import mediapipe as mp
+
+app = Flask(__name__)
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(
+        static_image_mode=False,
+        model_complexity=1,
+        smooth_landmarks=True,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5)
+
+cap = None
+
+def cap_real_time():
+    global cap
+        
+    if cap is None or not cap.isOpened():
+        cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        ret, img = cap.read()
+        if not ret:
+            break
+
+        # h, w, _ = img.shape
+        # target_w = int(h * 0.75)
+        # start_x = (w - target_w) // 2
+        # end_x = start_x + target_w
+        # img = img[:, start_x:end_x]
+
+        img = cv2.flip(img, 1)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_rgb.flags.writeable = False            
+        results = pose.process(img_rgb)
+        img.flags.writeable = True
+        if results.pose_landmarks:
+            mp_drawing.draw_landmarks(
+                img, 
+                results.pose_landmarks, 
+                mp_pose.POSE_CONNECTIONS,
+        mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+        mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+            )
+
+        ret, buffer = cv2.imencode('.jpg', img)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/start', methods=['POST'])
+def start():
+    return render_template('index.html')
+
+@app.route('/stop',methods=['POST'])
+def stop():
+    global cap
+    if cap is not None and 'cap' in globals():
+        if cap.isOpened():
+            cap.release()
+    return render_template('stop.html')
+
+@app.route('/cap_in_html')
+def cap_in_html():
+    return Response(cap_real_time(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=7337)
