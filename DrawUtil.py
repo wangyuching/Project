8ff4c9b472c109778a53f52ee94a1eab53c5ce5d
wang <5b1g0023@stuts.edu.tw>
2026-01-05 17:23:41 +0800

    20260105 from count_eating_medcine_use_count_eat_s2

--- DrawUtil.py
+++ DrawUtil.py
@@ -42,44 +42,40 @@ def calculate_angle(landmark1, landmark2, landmark3):
     return angle
 
 def detectPose(output_image, landmarks):
-    try:
 
-        # 兩嘴角中心點:嘴角左[9]、嘴角右[10]
-        mouth_center = (landmarks[mp_pose.PoseLandmark.MOUTH_LEFT.value] + landmarks[mp_pose.PoseLandmark.MOUTH_RIGHT.value]) / 2
-        cv2.circle(output_image, (int(mouth_center[0]), int(mouth_center[1])), 8, (0, 255, 0), -1)
+    # 兩嘴角中心點:嘴角左[9]、嘴角右[10]
+    mouth_center = (landmarks[mp_pose.PoseLandmark.MOUTH_LEFT.value] + landmarks[mp_pose.PoseLandmark.MOUTH_RIGHT.value]) / 2
+    cv2.circle(output_image, (int(mouth_center[0]), int(mouth_center[1])), 8, (0, 255, 0), -1)
 
-        # 左手肘角度:左肩[11]、左肘[13]、左腕[15]
-        left_elbow_angle = calculate_angle(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value], 
-                                           landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value], 
-                                           landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value])
+    # 左手肘角度:左肩[11]、左肘[13]、左腕[15]
+    left_elbow_angle = calculate_angle(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value], 
+                                        landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value], 
+                                        landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value])
 
-        # 右手肘角度:右肩[12]、右肘[14]、右腕[16]
-        right_elbow_angle = calculate_angle(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value], 
-                                           landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value], 
-                                           landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value])
+    # 右手肘角度:右肩[12]、右肘[14]、右腕[16]
+    right_elbow_angle = calculate_angle(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value], 
+                                        landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value], 
+                                        landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value])
 
-        # 左手中心點:左腕[15]、左小指[17]、左食指[19]、左姆指[21]
-        left_hand_center = np.array([landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value],
-                                     landmarks[mp_pose.PoseLandmark.LEFT_PINKY.value],
-                                     landmarks[mp_pose.PoseLandmark.LEFT_INDEX.value],
-                                     landmarks[mp_pose.PoseLandmark.LEFT_THUMB.value]]).mean(axis=0)
-        cv2.circle(output_image, (int(left_hand_center[0]), int(left_hand_center[1])), 8, (0, 0, 255), -1)
+    # 左手中心點:左腕[15]、左小指[17]、左食指[19]、左姆指[21]
+    left_hand_center = np.array([landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value],
+                                    landmarks[mp_pose.PoseLandmark.LEFT_PINKY.value],
+                                    landmarks[mp_pose.PoseLandmark.LEFT_INDEX.value],
+                                    landmarks[mp_pose.PoseLandmark.LEFT_THUMB.value]]).mean(axis=0)
+    cv2.circle(output_image, (int(left_hand_center[0]), int(left_hand_center[1])), 8, (0, 0, 255), -1)
 
-        # 計算左手中心點與嘴中心點距離
-        l_2_m_distance = np.linalg.norm(left_hand_center - mouth_center)
+    # 計算左手中心點與嘴中心點距離
+    l_2_m_distance = np.linalg.norm(left_hand_center - mouth_center)
 
-        # 右手中心點:右腕[16]、右小指[18]、右食指[20]、右姆指[22]
-        right_hand_center =  np.array([landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value],
-                                      landmarks[mp_pose.PoseLandmark.RIGHT_PINKY.value],
-                                      landmarks[mp_pose.PoseLandmark.RIGHT_INDEX.value],
-                                      landmarks[mp_pose.PoseLandmark.RIGHT_THUMB.value]]).mean(axis=0)
-        cv2.circle(output_image, (int(right_hand_center[0]), int(right_hand_center[1])), 8, (0, 0, 255), -1)
+    # 右手中心點:右腕[16]、右小指[18]、右食指[20]、右姆指[22]
+    right_hand_center =  np.array([landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value],
+                                    landmarks[mp_pose.PoseLandmark.RIGHT_PINKY.value],
+                                    landmarks[mp_pose.PoseLandmark.RIGHT_INDEX.value],
+                                    landmarks[mp_pose.PoseLandmark.RIGHT_THUMB.value]]).mean(axis=0)
+    cv2.circle(output_image, (int(right_hand_center[0]), int(right_hand_center[1])), 8, (0, 0, 255), -1)
 
-        # 計算右手中心點與嘴中心點距離
-        r_2_m_distance = np.linalg.norm(right_hand_center - mouth_center)
-
-    except Exception as e:
-        print(f"Error in detectPose: {e}")
+    # 計算右手中心點與嘴中心點距離
+    r_2_m_distance = np.linalg.norm(right_hand_center - mouth_center)
 
     output_image = cv2.flip(output_image, 1)
     
@@ -89,12 +85,12 @@ def detectPose(output_image, landmarks):
     cv2.putText(output_image, f'R_H to M Distance: {int(r_2_m_distance)}', (480, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)    
 
 
-    is_eating_medicine = False
-    if left_elbow_angle <= 60 and l_2_m_distance <= 100:
-        is_eating_medicine = True
-        cv2.putText(output_image, "Eat Medicine", (100, 400), cv2.FONT_HERSHEY_SIMPLEX, 3.5, (0, 255, 255), 7)
-    elif right_elbow_angle <= 60 and r_2_m_distance <= 100:
-        is_eating_medicine = True
-        cv2.putText(output_image, "Eat Medicine", (100, 400), cv2.FONT_HERSHEY_SIMPLEX, 3.5, (0, 255, 255), 7)
+    
+    # if left_elbow_angle <= 60 and l_2_m_distance <= 100:
+        
+    #     cv2.putText(output_image, "Eat Medicine", (100, 400), cv2.FONT_HERSHEY_SIMPLEX, 3.5, (0, 255, 255), 7)
+    # elif right_elbow_angle <= 60 and r_2_m_distance <= 100:
+        
+    #     cv2.putText(output_image, "Eat Medicine", (100, 400), cv2.FONT_HERSHEY_SIMPLEX, 3.5, (0, 255, 255), 7)
 
-    return output_image, is_eating_medicine
+    return output_image, left_elbow_angle, right_elbow_angle, l_2_m_distance, r_2_m_distance
--- app.py
+++ app.py
@@ -1,14 +1,80 @@
 from flask import Flask, render_template, Response
-from camera import camera_stream
+import cv2
+import time as t
+import DrawUtil
 
 app = Flask(__name__)
 
-camera_video = None
-def get_camera_video():
-    global camera_video
-    if camera_video is None:
-        camera_video = camera_stream()
-    return camera_video
+def cap_real_time():
+    # dont need to change values here
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1024)
+    cap.set(4, 768)
+    current_eat_state = "ready"
+    count = 0
+
+    eating_frame_count = 0
+    ACTION_THRESHOLD = 2 #fps
+    last_count_time = 0
+    COOLDOWN_SECONDS = 2  # seconds
+    # ------------------------------
+    while cap.isOpened():
+        ok, frame = cap.read()
+        if not ok:
+            continue
+        
+        frame_height, frame_width, _ = frame.shape
+        frame = cv2.resize(frame, (int(frame_width * (650 / frame_height)), 650))
+
+        frame, landmarks = DrawUtil.show_landmarks(frame, DrawUtil.pose)
+
+        # 偵測吃藥動作
+        if landmarks:
+            frame, left_elbow_angle, right_elbow_angle, l_2_m_distance, r_2_m_distance = DrawUtil.detectPose(frame, landmarks)
+        else:
+            cv2.putText(frame, "No Pose Detected", (80, 400), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 7)
+        ''' -------- if wanna put other thing, must after this line -------- '''            
+        
+        # 計算吃藥次數
+        # 判斷是否處於吃藥姿勢
+        is_eating_pose = (left_elbow_angle <= 60 and l_2_m_distance <= 100) or (right_elbow_angle <= 60 and r_2_m_distance <= 100)
+        # 判斷是否處於結束姿勢
+        is_resting_pose = (left_elbow_angle >= 90) or (right_elbow_angle >= 90)
+
+        # 重置計數
+        if count == 2:
+            count = 0
+
+        current_time = t.time()
+
+        if is_eating_pose:
+            eating_frame_count += 1
+            if eating_frame_count >= ACTION_THRESHOLD and(current_time - last_count_time) > COOLDOWN_SECONDS:
+                count += 1
+                current_eat_state = "Eating"
+                last_count_time = current_time
+                eating_frame_count = 0
+        else:
+            eating_frame_count = 0
+        
+        if is_resting_pose:
+            current_eat_state = "Detecting"    
+
+        cv2.putText(frame, f'Current State: {current_eat_state}', (30, 200), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 255), 3)
+        cv2.putText(frame, f'Count: {count}', (30, 250), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 255), 3)
+
+        # 顯示目前時間
+        current_time = t.localtime()
+        format_time = t.strftime("%Y-%m-%d %A %H:%M:%S", current_time)
+        now = format_time
+        cv2.putText(frame, f"{now}", (80, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 0), 3)  
+
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    cap.release()
+    cv2.destroyAllWindows()
 
 @app.route('/')
 def index():
@@ -16,15 +82,7 @@ def index():
 
 @app.route('/cap_in_html')
 def cap_in_html():
-    cam = get_camera_video()
-    return Response(cam.cap_real_time(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(cap_real_time(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/stop', methods=['POST'])
-def stop():
-    global camera_video
-    if camera_video:
-        camera_video.release()
-        camera_video = None
-    return "Camera stream stopped."
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=7337)
